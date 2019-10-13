64af23e9ba0c8c649cd6dbf2da2c24c2eb370683
Owen Hu <owenhu99@gmail.com>
2019-10-12 23:25:15 -0400

    Reformated java code, updated notes.txt.

--- FishTank/FishTank/app/src/main/java/uoft/csc207/fishtank/MainActivity.java
+++ FishTank/FishTank/app/src/main/java/uoft/csc207/fishtank/MainActivity.java
@@ -8,11 +8,13 @@ import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(new FishTankView(this));
-    }
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    requestWindowFeature(Window.FEATURE_NO_TITLE);
+    getWindow()
+        .setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    setContentView(new FishTankView(this));
+  }
 }
